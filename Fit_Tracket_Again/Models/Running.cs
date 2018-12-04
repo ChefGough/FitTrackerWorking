5b4e1874d6d8603769a3386c53597f04d44038f6
Alexander Gough <gougha3@my.erau.edu>
2018-12-04 08:41:24 -0500

    Added data for running page

---
+++ Fit_Tracket_Again/Models/Running.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fit_Tracket_Again.Models
+{
+    public class Running
+    {
+        public decimal Distance { get; set; }
+        public decimal Time { get; set; }
+        public string RunType { get; set; }
+    }
+}
